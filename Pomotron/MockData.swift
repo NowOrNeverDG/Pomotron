8244aa76d4b945d3fca4078674d533cf4ea7838d
NowOrNeverDG <447405890@qq.com>
2023-12-04 22:37:07 -0500

    update day chart

--- Pomotron/MockData.swift
+++ Pomotron/MockData.swift
@@ -9,6 +9,7 @@ import Foundation
 
 
 class MockData {
+    
     static var mockWeekDayArr: [[WeekDay]] {
         var weekDays1: [WeekDay] = []
         let calendar = Calendar.current
@@ -30,5 +31,61 @@ class MockData {
         }
         return [weekDays1,weekDays2]
     }
+    
+    static var mockTasksDic: [Date : [MockTask]] = [
+        Calendar.current.date(byAdding: .day, value: 0, to: Date())! : [createTask(0),createTask(0)],
+        Calendar.current.date(byAdding: .day, value: 1, to: Date())! : [createTask(1),createTask(1)],
+        Calendar.current.date(byAdding: .day, value: 2, to: Date())! : [createTask(2),createTask(2),createTask(2)],
+        Calendar.current.date(byAdding: .day, value: 3, to: Date())! : [createTask(3),createTask(3)],
+        Calendar.current.date(byAdding: .day, value: 4, to: Date())! : [createTask(4),createTask(4),createTask(4),createTask(4)],
+        Calendar.current.date(byAdding: .day, value: 6, to: Date())! : [createTask(6),createTask(6)],
+        Calendar.current.date(byAdding: .day, value: 7, to: Date())! : [createTask(7)],
+        Calendar.current.date(byAdding: .day, value: 8, to: Date())! : [createTask(8),createTask(8)],
+        Calendar.current.date(byAdding: .day, value: 9, to: Date())! : [createTask(9)],
+        Calendar.current.date(byAdding: .day, value: 10, to: Date())! : [createTask(10),createTask(10),createTask(10),createTask(10)],
+        Calendar.current.date(byAdding: .day, value: 11, to: Date())! : [createTask(11),createTask(11)],
+        Calendar.current.date(byAdding: .day, value: 12, to: Date())! : [createTask(12),createTask(12),createTask(12),createTask(12)],
+        Calendar.current.date(byAdding: .day, value: 14, to: Date())! : [createTask(14),createTask(14),createTask(14),createTask(14)],
+        Calendar.current.date(byAdding: .day, value: 15, to: Date())! : [createTask(15),createTask(15),createTask(15)],
+        Calendar.current.date(byAdding: .day, value: 16, to: Date())! : [createTask(16),createTask(16)],
+        Calendar.current.date(byAdding: .day, value: 17, to: Date())! : [createTask(17)],
+        Calendar.current.date(byAdding: .day, value: 18, to: Date())! : [createTask(18),createTask(18)],
+        Calendar.current.date(byAdding: .day, value: 19, to: Date())! : [createTask(19)],
+        Calendar.current.date(byAdding: .day, value: 20, to: Date())! : [createTask(20),createTask(20),createTask(20),createTask(20)],
+        Calendar.current.date(byAdding: .day, value: 21, to: Date())! : [createTask(21),],
+        Calendar.current.date(byAdding: .day, value: 24, to: Date())! : [createTask(24)],
+        Calendar.current.date(byAdding: .day, value: 25, to: Date())! : [createTask(25),createTask(25),createTask(25),createTask(25)],
+        Calendar.current.date(byAdding: .day, value: 26, to: Date())! : [createTask(26),createTask(26),createTask(26)],
+        Calendar.current.date(byAdding: .day, value: 28, to: Date())! : [createTask(28),createTask(28)],
+        Calendar.current.date(byAdding: .day, value: 29, to: Date())! : [createTask(29),createTask(29)]
+    ]
+}
+
+extension MockData {
+    private static func createTask(_ dayOffset: Int) -> MockTask {
+        var mockTask: MockTask = MockTask()
+        let calendar = Calendar.current
+        if let day = calendar.date(byAdding: .day, value: -dayOffset, to: Date()) {
+            let startTime = calendar.date(byAdding: .hour, value: 1, to: calendar.startOfDay(for: day)) ?? Date()
+            let endTime = calendar.date(byAdding: .hour, value: 2, to: calendar.startOfDay(for: day)) ?? startTime
+            mockTask = MockTask(addedDate: day, startTime: startTime, endTime: endTime)
+        }
+        return mockTask
+    }
+    
+}
+
+struct MockTask {
+    var id: String = UUID().uuidString
+    var title: String = "title"
+    var addedDate: Date = Date()
+    var startTime: Date = Date()
+    var endTime: Date = Date()
+}
+
+struct MockTasksForDate: Identifiable {
+    var id = UUID().uuidString
+    var day: Date
+    var tasksCount: Int
 }
 
--- Pomotron/View/StatsView/StatsView.swift
+++ Pomotron/View/StatsView/StatsView.swift
@@ -6,22 +6,37 @@
 //
 
 import SwiftUI
-import SwiftData
+import Charts
 
 struct StatsView: View {
     @State var statsType: StatsType = .PomoStats
     
+    
     var body: some View {
-        /// Segmneted Picker
-        PickerView()
-        
         /// Statistic View
-        if statsType == .PomoStats {
-            
-        } else {
-            
-        }
+        //title: 月pomo总数，周pomo总数，日pomo总数
         
+        //title: 默认最高pomo的time和量 + 摸了指定day的pomo和量
+        //每日完成pomo的量
+        
+        //title: 默认最高pomo的day和量 + 摸了指定day的pomo的day和量
+        //每周完成pomo的量
+        
+        //title: 默认最高pomo的数和量， 摸了指定pomo的数和量
+        //pomo tag分布 barmark3
+        
+        //title: 默认最高周pomo的数和量，摸了指定周pomo的数和量
+        //从这周起往前，向前一共八周的周pomo的数和量
+        VStack {
+            /// Segmneted Picker
+            PickerView()
+            
+            SumView()
+                .frame(maxHeight:100)
+            
+            DayStatsView()
+        }
+        .padding()
 
         
     }
@@ -39,20 +54,51 @@ struct StatsView: View {
     }
     
     @ViewBuilder
-    func PomoStatsView() -> some View {
-        //title: 月pomo总数，周pomo总数，日pomo总数
-        
-        //title: 默认最高pomo的time和量 + 摸了指定day的pomo和量
-        //每日完成pomo的量
-        
-        //title: 默认最高pomo的day和量 + 摸了指定day的pomo的day和量
-        //每周完成pomo的量
-        
-        //title: 默认最高pomo的数和量， 摸了指定pomo的数和量
-        //pomo tag分布 barmark3
-        
-        //title: 默认最高周pomo的数和量，摸了指定周pomo的数和量
-        //从这周起往前，向前一共八周的周pomo的数和量
+    func SumView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(lineWidth: 1)
+            
+            HStack {
+                VStack {
+                    Text("月 POMO")
+                    Text("120")
+                }
+                Spacer()
+                VStack {
+                    Text("周 POMO")
+                    Text("40")
+                }
+                Spacer()
+                VStack {
+                    Text("日 POMO")
+                    Text("8")
+                }
+            }
+            .padding()
+        }
+
+    }
+    
+    @ViewBuilder
+    func DayStatsView() -> some View {
+        VStack {
+            Chart(Array(MockData.mockTasksDic.keys).sorted().reversed(), id: \.self) { index in
+                LineMark(x: .value("日期", index.format("dd MMM")),
+                         y: .value("Pomo数", MockData.mockTasksDic[index]?.count ?? 0))
+                .interpolationMethod(.monotone)
+            }
+            .foregroundStyle(.green)
+            .chartScrollableAxes(.horizontal)
+            .chartYScale(domain: [0,10])
+            .chartXVisibleDomain(length: 5)
+            .chartYAxis {
+                AxisMarks(values:[0,4,8,12])
+            }
+            .chartXAxis {
+                AxisMarks(stroke: StrokeStyle(lineWidth: 0))
+            }
+        }
     }
 }
 
